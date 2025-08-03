a3764661ea43222f927128c1534375d84742eff3
fabio251176 <fabio251176@gmail.com>
2025-08-03 11:52:23 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,18 @@ provider "aws" {
   region = var.region
 }
 
+module "vpc" {
+  source      = "./modules/vpc"
+  cidr_block  = var.vpc_cidr_block
+  environment = var.environment
+}
+
 module "ec2_instance" {
   source        = "./modules/ec2"
   instance_name = "sre-demo"
   instance_type = "t2.micro"
   ami_id        = var.ami_id
+  vpc_id        = module.vpc.vpc_id
 }
 
 module "s3_bucket" {
@@ -14,8 +21,3 @@ module "s3_bucket" {
   bucket_name = "sre-demo-bucket-${var.environment}"
 }
 
-module "vpc" {
-  source      = "./modules/vpc"
-  cidr_block  = var.vpc_cidr_block
-  environment = var.environment
-}
