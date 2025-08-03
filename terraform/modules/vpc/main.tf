6ebf83886b9e12d2f34e91a2f09009b20d61eb42
fabio251176 <fabio251176@gmail.com>
2025-08-03 11:55:16 -0300

    Update main.tf

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,10 +1,11 @@
 
 resource "aws_vpc" "main" {
-  cidr_block           = var.vpc_cidr
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-
+  cidr_block = var.cidr_block
   tags = {
     Name = "vpc-${var.environment}"
   }
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
 }
